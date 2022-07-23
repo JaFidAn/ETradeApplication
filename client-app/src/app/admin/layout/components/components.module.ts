5fff8fffd0a10191670ac899039dfdeec47e08a7
Rasim Alagezov <r.alagezov@gmail.com>
2022-07-23 13:28:14 +0400

    AngularMaterial-Bootstrap-jQuery

--- client-app/src/app/admin/layout/components/components.module.ts
+++ client-app/src/app/admin/layout/components/components.module.ts
@@ -1,8 +1,10 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -13,7 +15,9 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatListModule
   ],
   exports: [
     HeaderComponent,
